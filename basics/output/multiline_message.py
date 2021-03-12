0ec6d59abf8f9f813d243e61139d30dbf63bfa94
repl.it user <replituser@example.com>
2021-03-12 10:21:56 +0000

    Added a code to display a simple message, a multiline messageage, a multiline message and a code to demonstrate escape characters as well as a characters as well as a code to display an ascii robot

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,5 @@
+# Display message to the standard output
+print("System Failure Imminent!")
+print("System reboot has been initiated...")
+# Updated code to diaplay a longer multiline message.
+print("\n...rebooting sensort system\n...rebooting output motors\n...rebooting hover engine\n\nSystem online.")
