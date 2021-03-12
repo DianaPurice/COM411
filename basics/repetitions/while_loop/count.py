9b6aa913b03604e7306838bc95d716a88a0c5fc8
repl.it user <replituser@example.com>
2021-03-12 14:06:06 +0000

    Added code to demonstrate the use of a while loop with counterle loop with counter.

---
+++ basics/repetitions/while_loop/count.py
@@ -0,0 +1,18 @@
+# Ask user for live cables to be avoided
+print("How many live cables should I avoid?")
+lc = int(input())
+
+# Print blank
+print()
+
+# Create variable to track the number of live cables avoided
+alc = 0
+
+# Use while operator to count how many cables have been avoided
+while alc < lc:
+  print("Avoiding...", end="")
+  alc +=1
+  print(" Done! {} live cables avoided!".format(alc))
+
+# Display final message
+print("\nAll live cables have been avoided.")
---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,14 @@
+# Ask user how many cables should beep avoid
+print("How many cables should I remove?")
+c = int(input())
+
+# Print blank space
+print()
+# Create variable for cables revoved
+cr = 0
+
+# Use a while loop to count cables removed
+while cr < c:
+  print("Removed cable.")
+  # increament the variable for cable removed
+  cr +=1
