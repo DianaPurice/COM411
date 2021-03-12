45f175d6e8beb8cbd5793a4bc76dcc5648060810
repl.it user <replituser@example.com>
2021-03-12 10:33:41 +0000

    Added code to display an ascii robot with dynamic eyes.

--- basics/input/user_input.py
+++ basics/input/user_input.py
@@ -1,6 +1,6 @@
 # display message to ask user for name
 print("What is your name human?")
-# Added code to read user response and variable
+# Added code to read user response and created variable
 name = input()
 # Display confirmation message
 print("\nNice to meet you, {}!".format(name))
