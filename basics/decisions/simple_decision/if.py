9c578a31d297c7751f5b5ecb8e829c5183bc3f63
repl.it user <replituser@example.com>
2021-03-12 11:00:31 +0000

    Added code to demonstrate the use of an if statement.

---
+++ basics/decisions/simple_decision/if.py
@@ -0,0 +1,14 @@
+# Ask user for the type of book
+print("What type of book should I read?")
+book = input()
+
+# Use if to verify user answer
+if book == "adventure":
+  print("\nI like adventure books!")
+
+# Display final message
+print("\nFinished reading book!")
+
+
+
+
