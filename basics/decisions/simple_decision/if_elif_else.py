c0c5c67b8f378b123c2c623f2a382f93d5746443
repl.it user <replituser@example.com>
2021-03-12 11:14:26 +0000

    Added code to demonstrate the use of an if/elif/else statement.

---
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -0,0 +1,18 @@
+# Ask user for direction to paint
+print("Towards which direction should I paint?")
+d = input()
+print()
+
+# Use if/elif/else to determinate the direction
+if d == "up":
+  print("\nI am painting in the upward direction!")
+elif d == "down":
+  print("\nI am painting in the downward direction!")
+elif d == "left":
+  print("\nI am painting in the leftward direction!")
+elif d == "right":
+  print("\nI am painting in the rightward direction!")
+else:
+  print("\nI do not understand this direction!")
+# Print final message
+print("\nI finnished painting!")
