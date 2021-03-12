a32679ceabba377517e9d50a1c5507150e3c9ef4
repl.it user <replituser@example.com>
2021-03-12 14:23:24 +0000

    Added code to demonstrate the use of a while loop with len funle loop with len function.

---
+++ basics/repetitions/while_loop/len.py
@@ -0,0 +1,14 @@
+# Display initial message
+print("Please enter a phrase:")
+p = input()
+
+# print blank
+print()
+
+# Create variable for bops
+b = 0
+
+# Read in user response and display message
+while b < len(p):
+  print("Bop ", end="")
+  b +=1
