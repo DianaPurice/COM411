b218775d86aa23385cdec8a5773faf19a17fc277
repl.it user <replituser@example.com>
2021-03-22 17:15:29 +0000

    ...

---
+++ basics/repetitions/for_loop/membership_operators.py
@@ -0,0 +1,16 @@
+# Ask user for phrase
+print("What phrase do you see?")
+p = input()
+
+# Display message
+print("\nReversing phrase...", "\nThe phrase is: ", end="")
+
+# Create variable
+r = ""
+
+# Use for loop to reverse phrase
+for ph in p:
+  r = ph + r
+
+# Display result
+print(r)
