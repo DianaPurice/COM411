7d3ec5316b9dc563c12421f0ae060608420ace6c
repl.it user <replituser@example.com>
2021-03-22 15:37:37 +0000

    Added coments and adjusted prints

--- basics/repetitions/while_loop/sum_user_numbers.py
+++ basics/repetitions/while_loop/sum_user_numbers.py
@@ -1,13 +1,17 @@
-print("How many numbers should i be summing?")
+# Ask user for how many numbers to be added
+print("How many numbers should I be summing?")
 nts = int(input())
 
+# Create variables to use while counting
 ns = 0
 t = 0
 
+# Use while loop to ask for the numbers and count
 while ns < nts:
-  print("please enter number:")
+  print(f"\nPlease enter number {ns} of {nts}: ")
   n = int(input())
   ns +=1
   t = t + n
 
-print(t)
+# Display result
+print(f"\nThe result is : {t}")
