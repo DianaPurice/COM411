f4cf6cdac8db6696e9316af1bfab36dd1422e458
repl.it user <replituser@example.com>
2021-03-22 19:00:27 +0000

    Added code to demonstrate the use of a user defined function.

---
+++ basics/functions/simple_function.py
@@ -0,0 +1,10 @@
+# Define function listen with 0 parameters
+def listen():
+  # Ask user for sound
+  print("\nWhat sound did I hear?")
+  s = input()
+
+  print(f"\nThat was a loud {s}!")
+
+# Call the function
+listen()
