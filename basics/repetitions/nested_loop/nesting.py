dc042c9899d88b9e75b9ba7515ecc0085624c462
repl.it user <replituser@example.com>
2021-03-22 17:37:53 +0000

    Added code to demonstrate the use of nested loop and decisions. of nested loops and decisions.

---
+++ basics/repetitions/nested_loop/nesting.py
@@ -0,0 +1,27 @@
+# Ask user for sequence
+print("Please enter a sequence formed of dashed and 2 markers:")
+s = input()
+
+# Ask UserWarning
+print("\nWhat characters are the markers?")
+m = input()
+
+# Display blank
+print()
+
+# Create variables
+m1 = -1
+m2 = -1
+
+# Use for loop to count number of dashes between markers
+for ch in range(0, len(s), 1):
+  l = s[ch]
+
+  if (l == m):
+    if (m1 == -1):
+      m1 = ch
+    else:
+      m2 = ch
+
+# Display result
+print(f"The distance between the markers is {m2 - m1 -1}.")
