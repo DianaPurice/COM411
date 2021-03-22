d0a0ce2f552ac5ba94bb7d0fb378bf244c18abae
repl.it user <replituser@example.com>
2021-03-22 19:33:44 +0000

    Added code to demonstrate the use of a user-defined function wd function with multiple parameters.

---
+++ basics/functions/function_with_parameters.py
@@ -0,0 +1,11 @@
+# Define function climb_ladder
+def climb_ladder(r_s, c_s):
+  # Compare the 2 parameters
+  if (r_s > c_s):
+    print("\nStill some way to go!")
+  else:
+    print("\nWe are almost there!")
+
+# Call the function
+climb_ladder(5, 2)
+climb_ladder(2, 5)
