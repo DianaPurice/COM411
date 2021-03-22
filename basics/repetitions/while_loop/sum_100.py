a30748fac77534d12a2072663db5844081fe5338
repl.it user <replituser@example.com>
2021-03-22 15:31:35 +0000

    Added coments to sum_100

--- basics/repetitions/while_loop/sum_100.py
+++ basics/repetitions/while_loop/sum_100.py
@@ -1,10 +1,14 @@
+# Display initial message
 print("Calculating the sum of the first nunbers...")
 
+# Create variables to use in counting
 n = 0
 t = 0
 
+# Use while loop to count
 while n <= 100:
   t = t + n
   n +=1
 
+# Display result
 print(t)
