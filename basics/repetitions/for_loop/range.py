e2e5da952e6f8a8815b38f50cf46c1f1b74f18e6
repl.it user <replituser@example.com>
2021-03-22 16:31:19 +0000

    Added code to demonstrate the use of a simple for loop.

---
+++ basics/repetitions/for_loop/range.py
@@ -0,0 +1,15 @@
+# Ask user for level of brigthness
+print("What level of brigthness is required?")
+b = int(input())
+
+# Display blank
+print()
+
+# Use for loop to display result
+for br in range(0, b, 2):
+  print("Beep: ", "□ " * br)
+  print("Bop:  ", "□ " * br)
+  print()
+
+# Display final message
+print("\nBrigtness adjusted!")
