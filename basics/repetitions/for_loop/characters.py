3621a8d9b4d910e55b8c815c493684a4d8968dcc
repl.it user <replituser@example.com>
2021-03-22 16:53:52 +0000

    Added code to demonstrate the use of for loops in string indexfor loops in string indexing.

---
+++ basics/repetitions/for_loop/characters.py
@@ -0,0 +1,10 @@
+# Ask user for markings
+print("What strange marking do you see?")
+m = input()
+
+# Print blank
+print("\nIdentifying...\n")
+
+# Use for loop to display character and index of character
+for ch in range(0, len(m), 1):
+  print("Index ", ch, ":", m[ch])
