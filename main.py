8719dfe150133f4cb82a804099ea17e500caf6c4
repl.it user <replituser@example.com>
2021-03-23 10:36:59 +0000

    change name

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-print("Hello World")
-
 import basics.output.simple_message as simple_message
 import basics.output.multiline_message as multiline_message
+import basics.output.escape_characters as escape_characters
+import basics.output.ascii_art as ascii_art
+import basics.input.user_input as user_input
+import basics.input.ascii_robot as ascii_robot
+import basics.input.data_types as data_types
+import basics.input.string_operators as string_operators
+import basics.input.review as input_review
+import basics.decisions.simple_decision.if as if_op
+import basics.decisions.simple_decision.if_else as if_else
 
 def run_block_a():
     print("Which program in 'Block A: Basics' do you wish to run?")
@@ -10,6 +17,32 @@ def run_block_a():
         simple_message.run()
     elif (response == "multiline_message"):
         multiline_message.run()
+    elif (response == "escape_characters"):
+        escape_characters.run()
+    elif (response == "ascii_art"):
+        ascii_art.run()
+    elif (response == "user_input"):
+        user_input.run()
+    elif (response == "ascii_robot"):
+        ascii_robot.run()
+    elif (response == "data_types"):
+        data_types.run()
+    elif (response == "string_operators"):
+        string_operators.run()
+    elif (response == "input_review"):
+        input_review.run()
+    elif (response == "if_op"):
+        if_op.run()
+    elif (response == "if_else"):
+        if_else.run()
+
+
+
+
+
+
+def run_block_b():
+  print("Which program in 'Block A: basics do you wish to run?'")
 
 
 def run():
@@ -26,4 +59,8 @@ def run():
         elif (response == "q"):
             break
         else:
+          print("Invalid option! Please try again.")
+
+run()
+
 
