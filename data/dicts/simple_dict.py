87eb0d6906741c25bcc90b8c74028b32e7f992bd
repl.it user <replituser@example.com>
2021-03-26 13:26:13 +0000

    Added code to demonstrate a simple dictionary.

---
+++ data/dicts/simple_dict.py
@@ -0,0 +1,14 @@
+# Create function pattern
+def pattern():
+  # Create dictionary sequences
+  sequences = {"Short Sequence":3, "Sequence":2, "Long Sequence":1}
+  # Return dictionary values
+  return sequences
+
+# Create function run
+def run():
+  # Display dictionary
+  print(pattern())
+
+# Call run function
+run()
